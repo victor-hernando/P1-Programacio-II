4c57ebb399c0f2a591e6a4d39b7eb7f81fc5a543
victor hernando mata <vhernando@edu.tecnocampus.cat>
2021-04-25 15:48:14 +0200

    Primeres implementacions als metodes de la classe Grid

--- ConsoleApp1/Source/P1Game/Blinky.cs
+++ ConsoleApp1/Source/P1Game/Blinky.cs
@@ -6,5 +6,6 @@ namespace TcGame
 {
     class Blinky : Item
     {
+        
     }
 }
--- ConsoleApp1/Source/P1Game/Item.cs
+++ ConsoleApp1/Source/P1Game/Item.cs
@@ -15,25 +15,5 @@ namespace TcGame
             sizeX = X;
             sizeY = Y;
         }
-        public static Item generateRandomType()
-        {
-           switch (rnd.Next(6))
-            {
-                case 0:
-                    return new Blinky();
-                case 1:
-                    return new Blinky;
-                case 2:
-                    return new Blinky;
-                case 3:
-                    return new Blinky;
-                case 4:
-                    return new Blinky;
-                case 5:
-                    return new Blinky;
-                default:
-                    return null;
-            }
-        }
   }
 }
